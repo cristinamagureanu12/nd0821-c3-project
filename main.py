3ee11c56468abd7a680bb6b48849ea8dafa799c2
Cristina Magureanu <cristina.magureanu12@gmail.com>
2022-05-04 21:54:47 +0300

    Add heroku needed files

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ import pandas as pd
 from starter.ml.model import inference
 from starter.ml.data import process_data
 from typing import Literal
-from fastapi.testclient import TestClient
 from joblib import load
 import numpy as np
+import os    
+    
+if "DYNO" in os.environ and os.path.isdir(".dvc"):    
+    os.system("dvc config core.no_scm true")    
+    if os.system("dvc pull") != 0:    
+        exit("dvc pull failed")    
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 app = FastAPI()
+
 
 class User(BaseModel):
     age: int
@@ -49,6 +56,7 @@ class User(BaseModel):
         'Outlying-US(Guam-USVI-etc)', 'Scotland', 'Trinadad&Tobago',
         'Greece', 'Nicaragua', 'Vietnam', 'Hong', 'Ireland', 'Hungary',
         'Holand-Netherlands']
+
 
 @app.get("/")
 async def get_items():
@@ -98,9 +106,9 @@ async def infer(user_data: User):
         "native-country",
     ]
     X, _, _, _ = process_data(
-                df_temp,
-                categorical_features=cat_features,
-                encoder=encoder, lb=lb, training=False)
+        df_temp,
+        categorical_features=cat_features,
+        encoder=encoder, lb=lb, training=False)
 
     pred = inference(model, X)
     y = lb.inverse_transform(pred)[0]
